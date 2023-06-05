88f82c90d8c03fb0614a51cf44d83e3526766b33
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-05 16:16:32 -0500

    recalibrated new avatar image with CSS

--- src/components/pages/About/index.jsx
+++ src/components/pages/About/index.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import avatar from '../images/avatar.jpg';
+import React from "react";
+import avatar from "../../../images/jenniferAvatar2.jpg";
 
 export default function About() {
-  const name = 'Jennifer Eckenrode';
+  const name = "Jennifer Eckenrode";
   return (
-    <div className="about-section">
-      <h1> Hello, my name is {name}</h1>
-      <div className="avatar-image">
-        <img src={avatar} alt="Jennifer Avatar" />
-      </div>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
-      </div>
+    <div>
+      <header className="about-Header">
+        <h1>
+          Hello, my name is {name}
+        </h1>
+        <div className="avatar-image">
+            <img src={avatar} alt="Jennifer Avatar" />
+          </div>
+      </header>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
+      </p>
     </div>
   );
 }
