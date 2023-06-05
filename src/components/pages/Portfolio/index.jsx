8cbfb76d3f6fd419cee46c31d0ac2bd05731cea4
Jennifer Eckenrode <j.eckenrode@me.com>
2023-06-05 17:02:47 -0500

    added some gifs of projects to display on portfolio, and linked the paths to my projectData.js file

--- src/components/pages/Portfolio/index.jsx
+++ src/components/pages/Portfolio/index.jsx
@@ -3,17 +3,26 @@ import projects from "../../projectData";
 
 export default function Portfolio() {
   return (
-    <section className="portfolio-Section">
+<section className="portfolio-Section">
       <header className="portfolio-Header">
         <h1>Jennifer Eckenrode Portfolio</h1>
       </header>
 
       {projects.map((project, index) => (
         <div key={index}>
-          <h2>{project.name}</h2>
+          <h2>
+            <a href={project.deployLink} target="_blank" rel="noopener noreferrer">
+              {project.name}
+            </a>
+          </h2>
           <p>{project.description}</p>
           <div>
             <img src={project.image} alt={project.name} />
+          </div>
+          <div>
+            <a href={project.repoLink} target="_blank" rel="noopener noreferrer">
+              Project Repository
+            </a>
           </div>
         </div>
       ))}
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,32 +1,44 @@
 const projects = [
     {
-        name: "Project 1",
-        description: "Description of Project 1",
-        link: "https://github.com/princessjenn/"
+        name: "Clifford's Dog Shelter",
+        description: "Developed a Full Stack web application for shelter management, allowing users to browse available dogs. Implemented privileged log-in credentials for shelter employees to update, delete, and create dog records.",
+        image: "../../../images/CliffordsDogShelter.gif",
+        repoLink: "https://github.com/princessjenn/Project-2",
+        deployLink: "https://cliffords-dog-shelter.herokuapp.com/"
     },
     {
-        name: "Project 2",
-        description: "Description of Project 2",
-        link: "https://github.com/princessjenn/"
+        name: "International Kanye",
+        description: "Using two web APIs, one to fetch a randomized Kanye West quote, one to translate to spanish. Also uses Local Storage to save user UI theme preference.",
+        image: "../../../images/InternationalKanye.gif",
+        repoLink: "https://github.com/princessjenn/Project-01",
+        deployLink: "https://princessjenn.github.io/Project-01/"
     },
     {
         name: "Project 3",
         description: "Description of Project 3",
-        link: "https://github.com/princessjenn/"
+        image: "",
+        repoLink: "https://github.com/princessjenn/",
+        deployLink: "" 
     },
     {
         name: "Project 4",
         description: "Description of Project 4",
-        link: "https://github.com/princessjenn/"
+        image: "",
+        repoLink: "https://github.com/princessjenn/",
+        deployLink: ""
     }, {
         name: "Project 5",
         description: "Description of Project 5",
-        link: "https://github.com/princessjenn/"
+        image: "",
+        repoLink: "https://github.com/princessjenn/",
+        deployLink: ""
     },
     {
         name: "Project 6",
         description: "Description of Project 6",
-        link: "https://github.com/princessjenn/"
+        image: "",
+        repoLink: "https://github.com/princessjenn/",
+        deployLink: ""
     },
 ];
 
